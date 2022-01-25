b06ba767ce05e4aed33776f5128f2624349007d1
armaBurton <armanarma@armaburton.com>
2022-01-24 22:32:18 -0800

    changed a few things, but I forget what, I was working on animal farm at the same time

--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -1,13 +1,12 @@
 import './ZodiacCard.css'
 
-export default function ZodiacCard(props) {
+export default function ZodiacCard({ name, dates }) {
   {/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
-
   return (
     <div className='zodiac-card'>
-      <img alt={props.name} src={`/images/${props.name}.png`} />
-      <span className='name'>{props.name}</span>
-      <span>{props.dates}</span>
+      <img alt={name} src={`/images/${name}.png`} />
+      <span className='name'>{name}</span>
+      <span>{dates}</span>
     </div>
   )
 }
